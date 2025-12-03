0dfe5d3b2f4be3676713eaef26cc0f1f26498292
Jacob Carpenter <github@jacobcarpenter.com>
2025-12-02 19:39:58 -0800

    day 02 pt1

--- 02/index.ts
+++ 02/index.ts
@@ -1,7 +1,7 @@
 import { createParser } from "../utility";
 
 if (import.meta.main) {
-	const file = Bun.file(`${import.meta.dir}/sample.txt`);
+	const file = Bun.file(`${import.meta.dir}/input.txt`);
 	const input = await file.text();
 
 	const data = input.split(",").map(
@@ -11,5 +11,23 @@ if (import.meta.main) {
 		}),
 	);
 
-	console.log(data);
+	const part1 = data
+		.flatMap(enumerateRange)
+		.filter(isInvalid)
+		.reduce((acc, curr) => acc + BigInt(curr), BigInt(0));
+
+	console.log({ part1 });
+}
+
+function isInvalid(value: number) {
+	const numberString = value.toString();
+	return (
+		numberString.length % 2 === 0 &&
+		numberString.substring(0, numberString.length / 2) ===
+			numberString.substring(numberString.length / 2)
+	);
+}
+
+function enumerateRange({ start, end }: { start: number; end: number }) {
+	return Array.from({ length: end - start }, (_, i) => i + start);
 }
