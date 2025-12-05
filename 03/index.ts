586f1155d73d89162bd3b42b2792b18a5bb6bb26
Jacob Carpenter <github@jacobcarpenter.com>
2025-12-04 21:33:44 -0800

    remove `import.meta.main` guard for untested modules

--- 03/index.ts
+++ 03/index.ts
@@ -1,48 +1,46 @@
-if (import.meta.main) {
-	const file = Bun.file(`${import.meta.dir}/input.txt`);
-	const input = await file.text();
-	const lines = input.split("\n");
+const file = Bun.file(`${import.meta.dir}/input.txt`);
+const input = await file.text();
+const lines = input.split("\n");
 
-	const data = lines.map((line) => line.split("").map((x) => Number(x)));
+const data = lines.map((line) => line.split("").map((x) => Number(x)));
 
-	const part1 = data
-		.map((bank) => {
-			const { value: tensValue, valueIndex: tensIndex } = bank
-				.slice(0, -1)
-				.reduce(
-					(acc, value, valueIndex) => (value > acc.value ? { value, valueIndex } : acc),
-					{ value: -1, valueIndex: -1 },
-				);
-			const ones = bank.slice(tensIndex + 1).reduce((acc, curr) => (curr > acc ? curr : acc));
-			return tensValue * 10 + ones;
-		})
-		.reduce((acc, curr) => acc + curr);
+const part1 = data
+	.map((bank) => {
+		const { value: tensValue, valueIndex: tensIndex } = bank
+			.slice(0, -1)
+			.reduce((acc, value, valueIndex) => (value > acc.value ? { value, valueIndex } : acc), {
+				value: -1,
+				valueIndex: -1,
+			});
+		const ones = bank.slice(tensIndex + 1).reduce((acc, curr) => (curr > acc ? curr : acc));
+		return tensValue * 10 + ones;
+	})
+	.reduce((acc, curr) => acc + curr);
 
-	const part2 = data
-		.map((bank) => {
-			const { digits } = Array.from<undefined>({ length: 12 }).reduce(
-				({ digits, start }, _, index, { length }) => {
-					const { value, valueIndex } = bank
-						.slice(start, -(length - 1 - index) || undefined)
-						.reduce(
-							(acc, value, valueIndex) =>
-								value > acc.value ? { value, valueIndex } : acc,
-							{ value: -1, valueIndex: -1 },
-						);
-					return { digits: [...digits, value], start: start + valueIndex + 1 };
-				},
-				{ digits: [] as number[], start: 0 },
-			);
+const part2 = data
+	.map((bank) => {
+		const { digits } = Array.from<undefined>({ length: 12 }).reduce(
+			({ digits, start }, _, index, { length }) => {
+				const { value, valueIndex } = bank
+					.slice(start, -(length - 1 - index) || undefined)
+					.reduce(
+						(acc, value, valueIndex) =>
+							value > acc.value ? { value, valueIndex } : acc,
+						{ value: -1, valueIndex: -1 },
+					);
+				return { digits: [...digits, value], start: start + valueIndex + 1 };
+			},
+			{ digits: [] as number[], start: 0 },
+		);
 
-			return digits.reduce(
-				(acc, curr, index, { length }) => acc + Math.pow(10, length - 1 - index) * curr,
-				0,
-			);
-		})
-		.reduce((acc, curr) => acc + curr);
+		return digits.reduce(
+			(acc, curr, index, { length }) => acc + Math.pow(10, length - 1 - index) * curr,
+			0,
+		);
+	})
+	.reduce((acc, curr) => acc + curr);
 
-	console.log({
-		part1,
-		part2,
-	});
-}
+console.log({
+	part1,
+	part2,
+});
--- 04/index.ts
+++ 04/index.ts
@@ -2,60 +2,58 @@ import { seq, range } from "../utility";
 
 type Grid<T> = { cells: T[]; width: number; height: number };
 
-if (import.meta.main) {
-	const file = Bun.file(`${import.meta.dir}/input.txt`);
-	const input = await file.text();
-	const grid = input.split("\n").reduce(
-		({ cells, width }, line, rowNumber) => {
-			return {
-				cells: [...cells, ...line.split("")],
-				width: width || line.length,
-				height: rowNumber + 1,
-			};
+const file = Bun.file(`${import.meta.dir}/input.txt`);
+const input = await file.text();
+const grid = input.split("\n").reduce(
+	({ cells, width }, line, rowNumber) => {
+		return {
+			cells: [...cells, ...line.split("")],
+			width: width || line.length,
+			height: rowNumber + 1,
+		};
+	},
+	{ cells: [] as string[], width: 0, height: 0 },
+);
+
+const part1 = enumerateGrid(seq(grid.height), seq(grid.width))
+	.filter(({ x, y }) => isAccessible(grid, x, y))
+	.toArray().length;
+
+// not immediately obvious how to purely functionally express part2, so ...
+let part2 = 0;
+let part2Grid = grid;
+let lastRemoved: number;
+
+do {
+	({ nextGrid: part2Grid, removed: lastRemoved } = enumerateGrid(
+		seq(grid.height),
+		seq(grid.width),
+	).reduce(
+		({ nextGrid, removed }, { x, y }) => {
+			if (isAccessible(part2Grid, x, y)) {
+				return {
+					nextGrid: setCell(nextGrid, x, y, "x"),
+					removed: removed + 1,
+				};
+			}
+
+			return { nextGrid, removed };
 		},
-		{ cells: [] as string[], width: 0, height: 0 },
+		{ nextGrid: part2Grid, removed: 0 },
+	));
+
+	part2 += lastRemoved;
+} while (lastRemoved);
+
+console.log({ part1, part2 });
+
+function isAccessible(grid: Grid<string>, x: number, y: number) {
+	return (
+		getCell(grid, x, y) === "@" &&
+		getNeighbors(grid, x, y)
+			.filter((curr) => curr === "@")
+			.toArray().length < 4
 	);
-
-	const part1 = enumerateGrid(seq(grid.height), seq(grid.width))
-		.filter(({ x, y }) => isAccessible(grid, x, y))
-		.toArray().length;
-
-	// not immediately obvious how to purely functionally express part2, so ...
-	let part2 = 0;
-	let part2Grid = grid;
-	let lastRemoved: number;
-
-	do {
-		({ nextGrid: part2Grid, removed: lastRemoved } = enumerateGrid(
-			seq(grid.height),
-			seq(grid.width),
-		).reduce(
-			({ nextGrid, removed }, { x, y }) => {
-				if (isAccessible(part2Grid, x, y)) {
-					return {
-						nextGrid: setCell(nextGrid, x, y, "x"),
-						removed: removed + 1,
-					};
-				}
-
-				return { nextGrid, removed };
-			},
-			{ nextGrid: part2Grid, removed: 0 },
-		));
-
-		part2 += lastRemoved;
-	} while (lastRemoved);
-
-	console.log({ part1, part2 });
-
-	function isAccessible(grid: Grid<string>, x: number, y: number) {
-		return (
-			getCell(grid, x, y) === "@" &&
-			getNeighbors(grid, x, y)
-				.filter((curr) => curr === "@")
-				.toArray().length < 4
-		);
-	}
 }
 
 function* enumerateGrid(ys: () => Iterable<number>, xs: () => Iterable<number>) {
